79074d7864ef7400d2d6b749fda8cdb67fd64136
christian-bromann <contact@christian-bromann.com>
2013-03-14 00:59:10 +0100

    improved test case

--- test/webdriver_test.js
+++ test/webdriver_test.js
@@ -1,17 +1,29 @@
+/*
+ * grunt-webdriver
+ * https://github.com/christianbromann/grunt-webdriver
+ *
+ * Copyright (c) 2013 Christian Bromann
+ * Licensed under the MIT license.
+ */
+
 'use strict';
 
 var driver;
 
-exports.name = "Hello World Test";
+exports.name = "Simple Github Test";
 exports.tests = [{
     
-    name: "first hello world test",
+    name: "checks if title contains the search query",
     func: function(done) {
 
+        var query = 'grunt-webdriver';
         exports.driver
             .click('.search a')
-            .setValue('.search-page-input','grunt-webdriver')
+            .setValue('.search-page-input',query)
             .click('#search_form .button')
+            .getTitle(function(title) {
+                buster.assertions.assert(title.indexOf(query) !== -1);
+            })
             .end(done);
         
     }}
