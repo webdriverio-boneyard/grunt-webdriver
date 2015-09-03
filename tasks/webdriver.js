969441a780fd18e14b6801e0ec3b9b8cff1609b9
christian-bromann <contact@christian-bromann.com>
2015-09-02 23:05:26 -0700

    spawn wdio runner with desired args

--- tasks/webdriver.js
+++ tasks/webdriver.js
@@ -3,6 +3,8 @@
 var path = require('path'),
     fs = require('fs'),
     dargs = require('dargs'),
+    split = require('split'),
+    through2 = require('through2'),
     deepmerge = require('deepmerge');
 
 module.exports = function(grunt) {
@@ -16,7 +18,8 @@ module.exports = function(grunt) {
         var opts = deepmerge(this.options({
             nodeBin: 'node',
             wdioBin: wdioBin,
-            args: {}
+            args: {},
+            output: false
         }), this.data);
 
         /**
@@ -38,8 +41,44 @@ module.exports = function(grunt) {
             keepCamelCase: true
         });
 
-        console.log(opts, args);
-        return done();
+        grunt.log.debug('spawn wdio with these attributes:\n', args.join('\n'));
+        var child = grunt.util.spawn({
+            cmd: opts.nodeBin,
+            args: args,
+            opts: {
+                stdio:'pipe'
+            }
+        }, function(error, result, code) {
+            grunt.log.debug('wdio testrunner finished with exit code', code);
+
+            if (error) {
+                grunt.log.error(String(result));
+            }
+
+            done();
+            done = null;
+        });
+
+        process.stdin.pipe(child.stdin);
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
+
+        // Write the result in the output file
+        if (!grunt.util._.isUndefined(opts.output) && opts.output !== false) {
+            grunt.log.debug('Output test result to: ' + opts.output);
+            grunt.file.mkdir(path.dirname(opts.output));
+
+            child
+                .stdout
+                .pipe(split())
+                .pipe(through2(function (chunk, encoding, callback) {
+                    if (!(/^Using the selenium server at/).test(chunk.toString())) {
+                        this.push(chunk + '\n');
+                    }
+                    callback();
+                }))
+                .pipe(fs.createWriteStream(opts.output));
+        }
 
     });
 
