cdee1f6f5c16403f8b844511b79cd70dc93a7c60
just-boris <just-boris@hotmail.com>
2016-03-21 22:48:09 +0300

    clear eval argv to avoid cyclic runs

--- tasks/webdriver.js
+++ tasks/webdriver.js
@@ -5,6 +5,20 @@ var path = require('path'),
     dargs = require('dargs'),
     deepmerge = require('deepmerge');
 
+function getExecArgv() {
+  var execArgv = process.execArgv
+  // Remove the -e switch to avoid fork bombing ourselves.
+  // https://github.com/nodejs/node/blob/v5.9.0/lib/child_process.js#L36-L43
+  if (process._eval != null) {
+    var index = execArgv.lastIndexOf(process._eval);
+    if (index > 0) {
+      execArgv = execArgv.slice();
+      execArgv.splice(index - 1, 2);
+    }
+  }
+  return execArgv;
+}
+
 module.exports = function(grunt) {
 
     grunt.registerMultiTask('webdriver', 'run wdio test runner', function() {
@@ -33,7 +47,7 @@ module.exports = function(grunt) {
             grunt.util.error('No config file found');
         }
 
-        var args = process.execArgv.concat([opts.wdioBin, opts.configFile]).concat(dargs(opts, {
+        var args = getExecArgv().concat([opts.wdioBin, opts.configFile]).concat(dargs(opts, {
             excludes: ['nodeBin', 'wdioBin'],
             keepCamelCase: true
         }));
