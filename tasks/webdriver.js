267f5e72815e348c1236b4ffc8b9dd3464e0d5bd
christian-bromann <contact@christian-bromann.com>
2016-05-05 23:57:06 -0700

    updated package to use webdriverio v4

--- tasks/webdriver.js
+++ tasks/webdriver.js
@@ -1,59 +1,27 @@
-'use strict';
+import path from 'path'
+import resolve from 'resolve'
+import merge from 'deepmerge'
 
-var path = require('path'),
-    fs = require('fs'),
-    dargs = require('dargs'),
-    deepmerge = require('deepmerge');
+module.exports = function (grunt) {
+    grunt.registerMultiTask('webdriver', 'run wdio test runner', function () {
+        const done = this.async()
+        const opts = merge(this.options(), this.data)
+        const Launcher = require(path.join(path.dirname(resolve.sync('webdriverio')), 'lib/launcher'))
 
-module.exports = function(grunt) {
-
-    grunt.registerMultiTask('webdriver', 'run wdio test runner', function() {
-
-        var done = this.async(),
-            base = process.cwd(),
-            wdioBin = path.join(base, 'node_modules', 'webdriverio', 'bin') + '/wdio';
-
-        var opts = deepmerge(this.options({
-            nodeBin: 'node',
-            wdioBin: wdioBin,
-            args: {}
-        }), this.data);
-
-        /**
-         * check webdriverio dependency
-         */
-        if (!fs.existsSync(opts.wdioBin)) {
-            grunt.util.error('Haven\'t found the WebdriverIO test runner');
+        if (typeof opts.configFile !== 'string') {
+            grunt.log.error('You need to define "configFile" property with the path to your wdio.conf.js')
+            return done(1)
         }
 
-        /**
-         * require config file
-         */
-        if(!opts.configFile) {
-            grunt.util.error('No config file found');
-        }
+        let wdio = new Launcher(opts.configFile, opts)
 
-        var args = process.execArgv.concat([wdioBin, opts.configFile]).concat(dargs(opts, {
-            excludes: ['nodeBin', 'wdioBin'],
-            keepCamelCase: true
-        }));
-
-        grunt.log.debug('spawn wdio with these attributes:\n', args.join('\n'));
-        grunt.util.spawn({
-            cmd: opts.nodeBin,
-            args: args,
-            opts: {
-                stdio: 'inherit'
-            }
-        }, function(error, result, code) {
-            grunt.log.debug('wdio testrunner finished with exit code', code);
-
-            if (error) {
-                grunt.log.error(String(result.stderr));
-            }
-
-            done(code === 0);
-            done = null;
-        });
-    });
-};
+        grunt.log.debug(`spawn wdio with these attributes:\n${opts}`)
+        return wdio.run().then(code => {
+            grunt.log.debug(`wdio testrunner finished with exit code ${code}`)
+            return done(code === 0)
+        }, e => {
+            grunt.log.error(`Something went wrong: ${e}`)
+            return done(false)
+        })
+    })
+}
